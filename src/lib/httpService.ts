3375f7c9c2744e73fc31695690099c155a39008e
ErthanDev <tranminhtri10213@gmail.com>
2025-12-04 16:49:13 +0700

    update: fix redirect

--- src/lib/httpService.ts
+++ src/lib/httpService.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
-
+const accessToken = localStorage.getItem("accessToken");
+console.log("Access Token in httpService:", accessToken);
 const instance = axios.create({
   baseURL: "https://api.eduguin.mtri.online/api",
+  headers: {
+    "Content-Type": "application/json",
+    ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+  },
 });
 
 export default instance;
